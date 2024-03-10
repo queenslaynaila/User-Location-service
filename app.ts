c4ff80542d8760ae4681b49f78370cf1191a4ba8
queenslaynaila <queenslayjema@gmail.com>
2024-03-10 17:56:39 +0300

    Remove doubel imports

--- app.ts
+++ app.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
