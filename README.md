5ca884039f79a02e51f8c7e28ca77c3eb5cf4dd7
queenslaynaila <queenslayjema@gmail.com>
2024-03-10 17:15:03 +0300

    Switch to api instead

---
+++ README.md
@@ -0,0 +1,9 @@
+# User Location Service
+
+## Description
+User Location Service is a Node.js application that provides geolocation data based on user IP addresses. It exposes a single endpoint for retrieving this information.
+
+## How to Use
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/queenslaynaila/User-Location-Service.git
--- app.ts
+++ app.ts
@@ -1,46 +1,58 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Request, Response } from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv'
 import { HttpError } from './middleware/errorMiddleware';
-import { Reader } from '@maxmind/geoip2-node';
+import { WebServiceClient } from '@maxmind/geoip2-node';
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+dotenv.config();
 
-const dbFilePath = './GeoLite2-City.mmdb';
-const readerPromise = Reader.open(dbFilePath);
+const acount = process.env.ACCOUNT_SID ?? 'default_value';
+const apiKey = process.env.API_KEY ?? 'default_value';
+
+const client = new WebServiceClient(
+  acount, apiKey  ,
+  { host: 'geolite.info' }
+)
 
 app.get('/user-location', async (req: Request, res: Response) => {
-  const ipAddress = req.ip!
-  const reader = await readerPromise;
-  const response = reader.city(ipAddress);
+  
+  const ipAddress = '196.207.143.83';
+  console.log(client)
+  const response = await client.city(ipAddress);
   console.log(response);
   const locationData = {
-    continent: response.continent?.names.en,
-    country: response.country?.names.en,
+    ip: ipAddress,
+    continent_code: response.continent?.code,
+    continent_name: response.continent?.names.en,
+    country_code: response.country?.isoCode,
+    country_name: response.country?.names.en,
+    isInEu: response.registeredCountry?.isInEuropeanUnion,
     city: response.city?.names.en,
     latitude: response.location?.latitude,
     longitude: response.location?.longitude,
-    postalCode: response.postal?.code,
+    postalCode: response.postal?.code || null,
     timezone: response.location?.timeZone,
   };
   res.json(locationData);
+
 });
 
-
-app.use(() => {
+app.use((req, res, next) => {
   throw new HttpError(404, 'Route Not found');
 });
 
-app.use((error: Error, req: Request, res: Response) => {
+app.use((error: Error, req: Request, res: Response ) => {
   if (error instanceof HttpError) {
     return res.status(error.statusCode).json({ error: error.message });
   }
-  console.error('Internal Server Error:', error);
   return res.status(500).json({ error: 'Internal Server Error' });
 });
 
