4f20a6c41e7f376a10a192bae4359ef3f106a943
queenslaynaila <queenslayjema@gmail.com>
2024-03-10 17:55:46 +0300

    Fix var case

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import morgan from 'morgan';
 import cors from 'cors';
@@ -17,43 +17,41 @@ dotenv.config();
 const acount = process.env.ACCOUNT_SID ?? 'default_value';
 const apiKey = process.env.API_KEY ?? 'default_value';
 
-const client = new WebServiceClient(
-  acount, apiKey  ,
-  { host: 'geolite.info' }
+const client = new WebServiceClient(acount,apiKey,{ host: 'geolite.info'}
 )
 
-app.get('/user-location', async (req: Request, res: Response) => {
-  
-  const ipAddress = '196.207.143.83';
-  console.log(client)
+app.get('/user-location', async (_req: Request, res: Response) => {
+  const ipAddress = '66.15.224.255';
   const response = await client.city(ipAddress);
-  console.log(response);
   const locationData = {
-    ip: ipAddress,
-    continent_code: response.continent?.code,
-    continent_name: response.continent?.names.en,
-    country_code: response.country?.isoCode,
-    country_name: response.country?.names.en,
+    ip: response.traits?.ipAddress,
+    continentCode: response.continent?.code,
+    continentName: response.continent?.names.en,
+    countryCode: response.country?.isoCode,
+    countryName: response.country?.names.en,
+    mobileCountryCode: response.traits?.mobileCountryCode || null,
     isInEu: response.registeredCountry?.isInEuropeanUnion,
-    city: response.city?.names.en,
+    city: response.city?.names.en || null,
     latitude: response.location?.latitude,
     longitude: response.location?.longitude,
     postalCode: response.postal?.code || null,
     timezone: response.location?.timeZone,
+    populationDensity: response.location?.populationDensity ||null
   };
   res.json(locationData);
-
 });
 
-app.use((req, res, next) => {
+app.use(() => {
   throw new HttpError(404, 'Route Not found');
 });
 
-app.use((error: Error, req: Request, res: Response ) => {
+// Global error handler
+/* eslint-disable @typescript-eslint/no-unused-vars */
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(error);
   if (error instanceof HttpError) {
     return res.status(error.statusCode).json({ error: error.message });
   }
   return res.status(500).json({ error: 'Internal Server Error' });
 });
-
 export default app;
