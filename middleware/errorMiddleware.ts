f77b6329179d88adb9d29955d9830486e8c05447
queenslaynaila <queenslayjema@gmail.com>
2024-03-14 11:32:52 +0300

    fix alignment

--- middleware/errorMiddleware.ts
+++ middleware/errorMiddleware.ts
@@ -1,9 +1,9 @@
 export class HttpError extends Error {
-    constructor(
-      public readonly statusCode: number,
-      message: string
-    ) {
-      super(message);
-    }
+  constructor(
+    public readonly statusCode: number,
+    message: string
+  ) {
+    super(message);
   }
+}
   
