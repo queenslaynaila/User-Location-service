48edc4c216e4bef1d628b67ac6e5ce687f0de010
queenslaynaila <queenslayjema@gmail.com>
2024-03-14 08:22:42 +0300

    Refactor code

--- routes/location.ts
+++ routes/location.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { sql } from './../db';
 import { HttpError } from '../middleware/errorMiddleware';
 
@@ -21,11 +21,12 @@ interface LocationData {
 export default (router: Router) => {
   router.get<string,Record<string, never>,LocationData,Record<string, never>,Record<string, never>>(
     '/user-location',
-    async (req: Request, res: Response) => {
+    async (req, res) => {
       const forwardedForHeader = req.headers['x-forwarded-for']! as string;
       const ipAddresses = forwardedForHeader.split(',');
       const realIpAddress = ipAddresses[0].trim();
-      const  country_code  = await SQL_GET_COUNTRY_CODE({network: realIpAddress}).one(new HttpError(500, 'Failed to get country code'));
+      const  country_code  = await SQL_GET_COUNTRY_CODE({network: realIpAddress})
+        .one(new HttpError(500, 'Failed to get country code'));
       res.json(country_code);
     }
   );
