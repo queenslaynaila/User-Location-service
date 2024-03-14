cce89dbdbbbe16f2668ec72a50cfea9f96a5b5c1
queenslaynaila <queenslayjema@gmail.com>
2024-03-14 08:40:16 +0300

    test limiter

--- rateLimit.ts
+++ rateLimit.ts
@@ -2,7 +2,7 @@ const databaseConfig = {
   dbFilePath: './GeoLite2-Country.mmdb',
   rateLimit: {
     windowMs: 60 * 60 * 1000, 
-    max: 100,
+    max: 5,
     standardHeaders: true,
     message: 'Too many requests from this IP, please try again later',
   },
